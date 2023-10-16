f73370dc02e3f5022742be38830d84efca54c223
yahya4321 <yahya.hamdi@esprit.tn>
2023-10-16 05:02:27 +0100

    Repository1

--- src/main/java/tn/esprit/tpfoyer/repository/IEtudiantRepository.java
+++ src/main/java/tn/esprit/tpfoyer/repository/IEtudiantRepository.java
@@ -1,11 +1,12 @@
 package tn.esprit.tpfoyer.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import tn.esprit.tpfoyer.entities.Etudiant;
 
 import java.util.Date;
 import java.util.List;
-
+@Repository
 public interface IEtudiantRepository extends CrudRepository<Etudiant,Long> {
 List<Etudiant> findAllByCinEqualsAndDateNaissance(long cin, Date date);
 }
--- src/main/java/tn/esprit/tpfoyer/repository/IReservationRepository.java
+++ src/main/java/tn/esprit/tpfoyer/repository/IReservationRepository.java
@@ -1,11 +1,12 @@
 package tn.esprit.tpfoyer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tn.esprit.tpfoyer.entities.Reservation;
 
 import java.util.Date;
 import java.util.List;
-
+@Repository
 public interface IReservationRepository extends JpaRepository<Reservation,String>{
     List<Reservation> findByanneeUniversitaireGreaterThan(Date dateAU);
 
