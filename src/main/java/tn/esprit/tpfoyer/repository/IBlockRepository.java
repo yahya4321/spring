c46e8987a4acff02a870eafb77764a1d43a2f2ec
yahya4321 <yahya.hamdi@esprit.tn>
2023-10-16 05:07:55 +0100

    Repository1

--- src/main/java/tn/esprit/tpfoyer/repository/IBlockRepository.java
+++ src/main/java/tn/esprit/tpfoyer/repository/IBlockRepository.java
@@ -3,9 +3,6 @@ package tn.esprit.tpfoyer.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.tpfoyer.entities.Bloc;
-import tn.esprit.tpfoyer.entities.Reservation;
-
-import java.util.Date;
 import java.util.List;
 @Repository
 public interface IBlockRepository extends JpaRepository<Bloc,Long>{
